31d253a854620be0e699a4e7c17bcd98e2e75b0c
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-05-04 11:51:24 +0200

    Update vite settings and packages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  let base = '/'
+
+  if (mode === 'test') {
+    base = '/project-management-react-app/'
+  }
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base,
+  }
 })
