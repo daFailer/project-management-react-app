d66055652fc3ccc5a6ca74042b7f7147f827ec32
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-05-11 17:37:39 +0200

    Create generic content components H2 and Paragraph

---
+++ src/components/H2.jsx
@@ -0,0 +1,7 @@
+const H2 = ({children}) => {
+  return (
+    <h2 className="text-xl font-bold text-stone-500 my-4">{children}</h2>
+  )
+}
+
+export default H2;
---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,7 @@
+const Paragraph = ({children}) => {
+  return (
+    <p className="text-stone-400 mb-4">{children}</p>
+  )
+}
+
+export default Paragraph;
