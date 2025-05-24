9ae619f6c7331e4924e7be87e2484aeb40aa80c0
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-05-24 14:54:57 +0200

    Fix SelectedProject HTML

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -14,13 +14,13 @@ const SelectedProject = ({project, onDeleteProject}) => {
           <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
           <Button onClick={() => onDeleteProject(project.id)}>Delete</Button>
         </div>
+        <p className="text-stone-400">
+          {formattedDate}
+        </p>
+        <p className="text-stone-600 mb-4 whitespace-pre-wrap">
+          {project.description}
+        </p>
       </header>
-      <p className="text-stone-400">
-        {formattedDate}
-      </p>
-      <p className="text-stone-600 mb-4 whitespace-pre-wrap">
-        {project.description}
-      </p>
       <p>TASKS</p>
     </div>
   )
